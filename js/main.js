66d18eb732cc4c00d6b08da25cdd3dbb0c9d4c1d
Nikola-rgb <bikola.johny@gmail.com>
2021-04-02 20:10:45 +0200

    odradjena sekcija blog

--- js/main.js
+++ js/main.js
@@ -15,3 +15,22 @@ $(document).ready(function(){
         $(this).addClass('active').siblings().removeClass('active');
     })
 });
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin:50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
