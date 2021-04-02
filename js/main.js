4b3b804076bcfa2f9e4864c2c246be3e9f4fd716
Nikola-rgb <bikola.johny@gmail.com>
2021-04-02 09:25:43 +0200

    promena radius slika

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
     $('.list').click(function(){
         const value = $(this).attr('data-filter');
        if (value == 'all'){
-           $('.itemBox').show('1000');
+           $('.itemBox').show(1000);
        }
        else{
-           $('.itemBox').not('.'+value).hide('1000');
-           $('.itemBox').filter('.'+value).show('1000');
+           $('.itemBox').not('.'+value).hide(1000);
+           $('.itemBox').filter('.'+value).show(1000);
        }
     });
     $('.list').click(function(){
