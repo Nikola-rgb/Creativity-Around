bed39229cc91068f5b16c3481b8c762328303b89
Nikola-rgb <bikola.johny@gmail.com>
2021-04-04 17:20:59 +0200

    sekcija say i ostale sekcije

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 
 
+
+
 $(document).ready(function(){
     $('.list').click(function(){
         const value = $(this).attr('data-filter');
        if (value == 'all'){
-           $('.itemBox').show(1000);
+           $('.itemBox').show('1000');
        }
        else{
-           $('.itemBox').not('.'+value).hide(1000);
-           $('.itemBox').filter('.'+value).show(1000);
+           $('.itemBox').not('.'+value).hide('1000');
+           $('.itemBox').filter('.'+value).show('1000');
        }
     });
     $('.list').click(function(){
         $(this).addClass('active').siblings().removeClass('active');
-    })
+    });
+
 });
 
+
+
+// Carousel section Blog
 
 $('.owl-carousel').owlCarousel({
     loop:true,
@@ -34,3 +40,44 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+
+
+// Slider Say Section
+
+var slides = document.querySelector('.slider-items').children;
+var nextSlide = document.querySelector(".next");
+var prevSlide = document.querySelector(".prev");
+var totalSlides = slides.length;
+var index = 0;
+
+nextSlide.onclick=function () {
+    next("next");
+}
+prevSlide.onclick=function () {
+    next("prev");
+}
+
+function next(direction){
+
+    if(direction=="next"){
+       index++;
+        if(index==totalSlides){
+         index=0;
+        }
+    } 
+    else{
+            if(index==0){
+             index=totalSlides-1;
+            }
+            else{
+             index--;
+            }
+     }
+ 
+   for(i=0;i<slides.length;i++){
+           slides[i].classList.remove("active");
+   }
+   slides[index].classList.add("active");     
+ 
+ };
